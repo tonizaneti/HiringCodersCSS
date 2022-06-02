b9c4daede6b18b7c04e6e615a0d375a9bf01e39d
Antonio Delvair Zaneti <antonio.zaneti@gmail.com>
2022-06-02 16:32:36 -0300

    Add files via upload

---
+++ Projeto/Semana04/app2.js
@@ -0,0 +1,51 @@
+/*
+O que é NPM?
+É o gerenciador de pacotes do NODE permitindo a busca, instalação, gerenciamento, criação e publicação de pacotes.
+
+COMPONENTES NPM
+website
+	Busca e análise de pacotes
+cli
+	Comandos de terminal, mais utilizado
+registry	
+	Local onde ficam armazenados os pacotes.
+	registry.npmjs.org
+	armazenar todos os pacotes disponíveis no NPM
+	acesso: através do NPM CLI
+
+
+PRÁTICA DE NPM
+    Arquitetura
+
+    Instalar e Configurar npm Init
+*/
+
+//Incluindo uma Biblioteca 
+//(Pacote HTTP)
+const http = require('http');
+// (Pacote URL)
+const url = require('url');
+// Pacote queryString
+const queryString = require('query-string');
+
+//Definição de Endereço URL
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+// Implementação da Regra de Negócio
+const server = http.createServer((req, res)=>{
+
+    // Pegar a Pergunta na URL
+    console.log(req.url);
+    const params = queryString.parse (url.parse(req.url, true).search);
+    console.log(params);
+    // Verificar a Pergunta e Escolher uma Resposta
+    // Retornar a resposta escolhida
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World Leia');
+});
+server.listen(port, hostname,()=>{
+    console.log(`Server runing at http://${hostname}:${port}`);
+});
---
+++ Projeto/Semana04/dev.js
@@ -0,0 +1,12 @@
+let alunos = ["Diogo","Carlos","Danilo","Pedro","César"];
+
+let contador = 0;
+
+while ( contador < alunos.length ) {
+
+    let aluno_nome = alunos[contador];
+
+    console.log( "Aluno: " + aluno_nome );
+    contador++;
+
+}
